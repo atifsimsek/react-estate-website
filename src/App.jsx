9788947a28fd1e00e8ba5363be1686b2926c5013
Atıf <dev.atifsimsek@gmail.com>
2023-07-07 23:13:08 +0300

    slider completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Companies from "./components/Companies";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Residencies from "./components/Residencies";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
+      <Residencies />
     </div>
   );
 }
